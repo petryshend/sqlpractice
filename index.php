ec810ef7b5c1e6ee7e03e67b0e0f484d7ab161c3
petryshend <petryshend@gmail.com>
2015-05-19 16:33:14 +0300

    Fix bug in tasks table structure. Solve second task

--- index.php
+++ index.php
@@ -19,4 +19,17 @@ $exercises[] = $ex;
 
 /******************************************/
 
+$stmt = $conn->prepare('SELECT Name, Price FROM Products');
+$stmt->execute();
+$rows = $stmt->fetchAll(); 
+
+$ex = [
+    'task' => 'Select the names and the prices of all the products in the store.',
+    'rows' => $rows,
+];
+
+$exercises[] = $ex;
+
+/******************************************/
+
 render('main', ['exercises' => $exercises]);
