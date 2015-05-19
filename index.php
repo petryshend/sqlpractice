512fb09bc49dc5578bb7b87d2a2ed0e2fb8e0e51
petryshend <petryshend@gmail.com>
2015-05-19 16:29:34 +0300

    Base tasks structure initialized. First task solved.

--- index.php
+++ index.php
@@ -2,4 +2,21 @@
 
 require_once 'bootstrap.php';
 
-render('main');
+$exercises = [];
+
+/*****************************************/
+
+$stmt = $conn->prepare('SELECT Name FROM Products');
+$stmt->execute();
+$rows = $stmt->fetchAll(); 
+
+$ex = [
+    'task' => 'Select the names of all the products in the store.',
+    'rows' => $rows,
+];
+
+$exercises[] = $ex;
+
+/******************************************/
+
+render('main', ['exercises' => $exercises]);
